196a956190d361a3f4247f5ce0846f249480cab6
unknown <2412130118@ACADEMICO.IESB.BR>
2026-08-21 11:03:11 -0300

    adiciona grafos aula02

---
+++ aulas/aula02/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "grafo_matriz.h"
+
+int main() {
+   
+    GrafoMatriz grafo;
+    int numero_vertices = 8;
+
+    inicializar(&grafo, numero_vertices);
+
+    inserir_aresta(&grafo, 0, 1);
+    inserir_aresta(&grafo, 0, 2);
+    inserir_aresta(&grafo, 0, 3);
+    inserir_aresta(&grafo, 1, 4);
+    inserir_aresta(&grafo, 1, 5);
+    inserir_aresta(&grafo, 2, 3);
+    inserir_aresta(&grafo, 2, 6);
+    inserir_aresta(&grafo, 3, 6);
+    inserir_aresta(&grafo, 7, 4);
+    inserir_aresta(&grafo, 7, 5);
+    inserir_aresta(&grafo, 7, 6);
+
+    inicializar(&grafo, 3);
+    inserir_arco(&grafo, 0, 1);
+    inserir_arco(&grafo, 1, 2);
+    inserir_arco(&grafo, 2, 0);
+
+    printf("Matriz de Adjencencia\n");
+    exibir_matriz(&grafo);
+
+    return 0;
+}
